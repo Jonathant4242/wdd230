7a164c46ddeb92c8944d6ff45950fd5b482161eb
John T <91383892+Jonathant4242@users.noreply.github.com>
2023-03-01 22:08:08 -0800

    updated date.js and added div

--- chamber/script/date.js
+++ chamber/script/date.js
@@ -1,10 +1,16 @@
-// header current date
-const datefield = document.querySelector(".date")
-const datefieldUk = document.querySelector("aside");
+const datefield = document.querySelector(".date");
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-Uk", { dateStyle: "full" }).format(
-  now
-);
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(now);
 
+// display the full date in the header
 datefield.innerHTML = `<em>${fulldate}</em>`;
-// datefieldUk.innerHTML = `<em>${fulldate}</em>`;
+
+// show the banner on Mondays or Tuesdays only
+const dayOfWeek = now.getDay();
+const banner = document.querySelector("#banner");
+
+if (dayOfWeek === 1 || dayOfWeek === 2) {
+  banner.style.display = "block";
+} else {
+  banner.style.display = "none";
+}
