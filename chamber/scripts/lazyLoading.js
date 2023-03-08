f71f17614d9d666c0e9a6613ef69be049455c129
John T <91383892+Jonathant4242@users.noreply.github.com>
2023-03-07 20:13:18 -0800

    more trouble shooting.

--- chamber/scripts/lazyLoading.js
+++ chamber/scripts/lazyLoading.js
@@ -1,13 +1,18 @@
- //Selecting all the images with data-src attribute.
+//Selecting all the images with data-src attribute.
 let loadPictures = document.querySelectorAll('img[data-src]');
 
-//loading the images removing the attrbute data-src
+//loading the images removing the attribute data-src
 const loadImg = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
+
+// Set the initial `src` attribute value for all images to the placeholder image URL.
+loadPictures.forEach((img) => {
+  img.setAttribute('src', 'http://placekitten.com/300/300');
+});
 
 // Parameters for the image to load on the window.
 const imgPerameters = {
@@ -21,10 +26,7 @@ if ('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
-        // Check if the image's `src` attribute is the basic image URL.
-        if (item.target.getAttribute('src') !== item.target.getAttribute('data-src')) {
-          loadImg(item.target);
-        }
+        loadImg(item.target);
         observer.unobserve(item.target);
       }
     });
