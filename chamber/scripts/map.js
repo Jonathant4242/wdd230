c1fb21af99c3cb6e8cdd2bc66fcfe493f3f054c0
John T <91383892+Jonathant4242@users.noreply.github.com>
2023-03-25 21:35:05 -0700

    lets go! contact is complete

--- chamber/scripts/map.js
+++ chamber/scripts/map.js
@@ -2,6 +2,7 @@
 function initMap() {
   var mapImg = document.createElement('img');
   mapImg.src = 'https://maps.googleapis.com/maps/api/staticmap?center=-34.18029061378806,-71.40025197543089&zoom=18&size=400x400&maptype=satellite&key=AIzaSyAODXu20Nych4V2u-svAD8O6IsFJzG9d5E';
+  mapImg.classList.add('contact_map'); 
   document.getElementById('map').appendChild(mapImg);
 }
 
